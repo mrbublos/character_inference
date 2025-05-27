28fc60fb0e600d5873237652b42eed1b98dc44f8
mrbublos <skrendel@gmail.com>
2025-05-27 22:54:22 +0300

    bazel build

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-export VERSION=1.16
-docker buildx build --platform linux/amd64 -t skrendelauth/inference:$VERSION -f docker/Dockerfile .
-docker push skrendelauth/inference:$VERSION
+#export VERSION=1.16
+#docker buildx build --platform linux/amd64 -t skrendelauth/inference:$VERSION -f docker/Dockerfile .
+#docker push skrendelauth/inference:$VERSION
+
+bazel build //:inference_embedded
