aa086d447bd58976e87989cc868da0b4414a196b
mrbublos <skrendel@gmail.com>
2025-05-29 20:17:08 +0300

    build fix

--- push_docker.sh
+++ push_docker.sh
@@ -5,15 +5,15 @@ wget https://github.com/bazelbuild/bazelisk/releases/download/v1.20.0/bazelisk-l
 chmod +x bazelisk-linux-amd64
 sudo cp ./bazelisk-linux-amd64 /usr/local/bin/bazel
 
-docker login -u skrendelauth
-
 git clone https://github.com/mrbublos/character_inference.git
 
 cd character_inference
 
 apt-get install rsync -y
 
-rsync -ah --progress /workspace/ht/models--black-forest-labs--flux.1-dev hf/
-rsync -ah --progress /workspace/lora_styles hf/
+rsync -ah --progress /workspace/ht/models--black-forest-labs--flux.1-dev hf/model/
+rsync -ah --progress /workspace/lora_styles hf/styles
+
+docker login -u skrendelauth
 
 bazel build //:push_inference_embedded
